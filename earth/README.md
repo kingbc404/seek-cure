cad1e6b1d4bb08def8a0dd0a0130763be0f24256
King BC <kingbc404@gmail.com>
2020-07-26 01:27:34 +0900

    EARTH - README 추가

---
+++ earth/README.md
@@ -0,0 +1,2 @@
+## JSON 한글 꺠짐 방지
+#### ensure_ascii=False 추가
--- earth/main.py
+++ earth/main.py
@@ -3,10 +3,10 @@ from sanic.response import json
 
 app = Sanic()
 
-
 @app.route("/")
-async def test(request):    
-    return json({"hello": "안녕"},ensure_ascii=False)
+async def test(request):
+    
+    return json({"hello": "안녕"}, ensure_ascii=False)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, workers=4)
+    app.run(host="0.0.0.0", port=8080)
